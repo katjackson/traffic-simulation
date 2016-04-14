df038762cc54285ce77386d7f3e1ba7491f2d948
Greg Johnston <greg.a.johnston@gmail.com>
2016-04-14 12:29:01 -0400

    Added a car class

---
+++ car.py
@@ -0,0 +1,9 @@
+class Car:
+
+    def __init__(self, position):
+        self.car_length = 5
+        self.max_speed = 100/3
+        self.accelerate = 2
+        self.decelerate = 2
+        self.decel_chance = 0.1
+        self.position = position
