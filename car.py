cbae8768681c11b7b7e6e179dee31ed1f1416257
Greg Johnston <greg.a.johnston@gmail.com>
2016-04-14 13:51:08 -0400

    Added accel/deccel tests and gitignore

--- car.py
+++ car.py
@@ -1,9 +1,28 @@
+import random
+
+
 class Car:
 
-    def __init__(self, position):
+    def __init__(self):
         self.car_length = 5
         self.max_speed = 100/3
-        self.accelerate = 2
-        self.decelerate = 2
-        self.decel_chance = 0.1
-        self.position = position
+        self.acceleration = 2
+        self.decceleration = 2
+        self.position = 0
+        self.speed = 0
+
+    def choose_speed_change(self):
+        if random.random() < .1:
+            self.deccelerate()
+        else:
+            self.accelerate()
+
+    def accelerate(self):
+        self.speed += 2
+        if self.speed > self.max_speed:
+            self.speed = self.max_speed
+
+    def deccelerate(self):
+        self.speed -= 2
+        if self.speed < 0:
+            self.speed = 0
--- car_tests.py
+++ car_tests.py
@@ -1,4 +1,35 @@
-from car import car
+from car import Car
+
+test_car = Car()
+
 
 def test_car_is_5m_long():
-    pass
+    assert test_car.car_length == 5
+
+
+def test_set_max_speed():
+    assert test_car.max_speed == 100/3
+
+
+def test_accelerate_max():
+    test_car.speed = 100/3 - 1
+    test_car.accelerate()
+    assert test_car.speed == 100/3
+
+
+def test_accelerate():
+    test_car.speed = 5
+    test_car.accelerate()
+    assert test_car.speed == 7
+
+
+def test_deccelerate():
+    test_car.speed = 5
+    test_car.deccelerate()
+    assert test_car.speed == 3
+
+
+def test_deccelerate_min():
+    test_car.speed = 1
+    test_car.deccelerate()
+    assert test_car.speed == 0
